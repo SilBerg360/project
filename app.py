fe12e6d01abbe44b78d042be5310861f7b11afad
SilBerg360 <siljessevandenberg@gmail.com>
2025-09-09 21:18:01 +0200

    Making FijndBrein Railway ready

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
-from flask import Flask, redirect, render_template, request, jsonify # type: ignore
-from sqlalchemy import create_engine, MetaData, Table, Select, bindparam, select # type: ignore
+from flask import Flask, redirect, render_template, request, jsonify
+from sqlalchemy import create_engine, MetaData, Table, Select, bindparam, select
 
 app = Flask(__name__)
 
@@ -44,8 +44,4 @@ def fijnd():
             else:
                 return render_template("error.html", reason="Invalid input")
         return render_template("fijnd.html", brain=brain)
-    
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
     
